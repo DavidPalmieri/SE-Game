7a6a1aeee2ada3f7120e6ed35beaee93adfd21db
Logan Jones <Joneslo15@Students.ecu.edu>
2018-11-01 17:05:13 -0400

    Enemy damage script(Alpha version)

---
+++ SoftwareProject/Assets/Script/Enemy/EnemyHealthManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+    public int MaxHealth;
+    public int CurrentHealth;
+
+    // initiation of health
+    private void Start(){
+        CurrentHealth = MaxHealth;
+    }
+    //update called once per frame
+    private void Update(){
+        if(CurrentHealth <= 0) {
+            Destroy(gameObject);
+        }
+    }
+    public void HurtEnemy (int damageDelt){
+        CurrentHealth -= damageDelt;
+    }
+    public void SetMaxHealth()
+    {
+        CurrentHealth = MaxHealth;
+    }
+}
