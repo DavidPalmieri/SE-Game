2366bc6274487147c1b10205574527c6057329d5
Logan Jones <Joneslo15@Students.ecu.edu>
2018-11-24 15:16:57 -0500

    Changes to the health bar and the tester used for the health bar

---
+++ SoftwareProject/Assets/Script/TestingHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class TestingHandler : MonoBehaviour {
+
+    [SerializeField] private HealthBar healthbar;
+    //this testing script will ensure the health bar can reduce if damage is taken
+    private void Start()
+    {
+        float health = 1f;
+        FunctionPeriodic.Create(() =>
+        {
+            if (health > 0)
+            {
+                health -= .01f;
+                healthbar.SetSize(health);
+            }
+        }, 0.03f);
+    }
+}
