7e18dc7236c248be0d2399bc7ab4da960b6176de
Logan Jones <Joneslo15@Students.ecu.edu>
2018-11-25 22:02:51 -0500

    minor changes to testing handler

--- SoftwareProject/Assets/Script/TestingHandler.cs
+++ SoftwareProject/Assets/Script/TestingHandler.cs
@@ -5,7 +5,7 @@ using CodeMonkey.Utils;
 
 public class TestingHandler : MonoBehaviour {
 
-    [SerializeField] private HealthBar healthbar;
+    [SerializeField] private HealthBar healthBar;
     //this testing script will ensure the health bar can reduce if damage is taken
     private void Start()
     {
@@ -15,7 +15,19 @@ public class TestingHandler : MonoBehaviour {
             if (health > 0)
             {
                 health -= .01f;
-                healthbar.SetSize(health);
+                healthBar.SetSize(health);
+            }
+            if (health < -.3f) {
+                // flashing if under 30% health
+                if ((health * 100f) % 3 == 0)
+                {
+                    healthBar.SetColor(Color.white);
+                }
+                else
+                {
+                    healthBar.SetColor(Color.red);
+                }
+                
             }
         }, 0.03f);
     }
